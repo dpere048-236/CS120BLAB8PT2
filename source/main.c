76acaabf8c63715c60300e81cfbea36c279de401
David Perez <dpere048@wch136-05.cs.ucr.edu>
2019-11-04 05:44:40 -0800

    done part 2

--- source/main.c
+++ source/main.c
@@ -1,23 +1,35 @@
-/*	Author: dpere048
+/*	Author: David Perez  dpere048
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab6 #  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-
+void ADC_init() {
+	ADCRSA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF; //initialize DDRA as inputs
+	DDRB = 0xFF; PORTB = 0x00; //init port B to 0s and outputs
+	DDRD - 0xFF; PORTD = 0x00;
+//	TimerSet(300); //leds timer light for 300ms
+//	TimerOn();
+	ADC_init();
     /* Insert your solution below */
     while (1) {
-
+	unsigned short x = ADC;
+	unsigned char leds8 = (char)x;
+	PORTB = leds8;
+	unsigned char leds2 = (char)(x >> 8);
+	PORTD = leds2;
     }
-    return 1;
+    return 0;
 }
